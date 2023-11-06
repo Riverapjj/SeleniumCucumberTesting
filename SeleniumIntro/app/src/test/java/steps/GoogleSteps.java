240c85a47f5509cb488776fe78c162283c736eb9
Josué Rivera <keko.rai.jr@gmail.com>
2023-11-05 22:46:00 -0600

    SeleniumIntro

---
+++ SeleniumIntro/app/src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,11 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+
+public class GoogleSteps {
+    
+    @Given("^I am on the Google search page$")
+    public void navigateToGoogle(){
+
+    }
+}
